01f80ebb68df598b7848c0dda73036d9e675a6e9
Prathiba Kamle <167279196+Prathiba-Mii@users.noreply.github.com>
2026-03-11 19:23:23 +0530

    Fix auth URLs to production

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -11,7 +11,7 @@ function Signup() {
 
   const handleSignup = async () => {
     try {
-      const res = await axios.post('http://localhost:5000/api/auth/signup', {
+      const res = await axios.post('https://smarthire-backend-8hgc.onrender.com/api/auth/signup', {
         name, email, password
       });
       localStorage.setItem('token', res.data.token);
